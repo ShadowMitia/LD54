589a99b01713b9b611351f6ba5e6a69e4308c814
Dimitri Belopopsky <dimitri@belopopsky.com>
2023-10-01 11:59:01 +0200

    Add ingredient and inventory system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::{prelude::*, sprite::collide_aabb::collide};
 
+#[derive(Component)]
+struct CollisionBox(Vec3);
+
+#[derive(Component)]
+struct TriggerBox(Vec3);
+
 fn main() {
     App::new()
         .add_event::<OnGroundEvent>()
@@ -16,6 +22,7 @@ fn main() {
             )
                 .chain(),
         )
+        .add_systems(Update, (trigger_ingredient_system, teller_system))
         .run();
 }
 
@@ -49,6 +56,7 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Collision,
+        CollisionBox(Vec3::new(2000.0, 60.0, 0.0)),
     ));
 
     // Teller
@@ -63,6 +71,9 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Collision,
+        CollisionBox(Vec3::new(24.0, 35.0, 0.0)),
+        Teller,
+        TriggerBox(Vec3::new(30.0, 36.0, 0.0)),
     ));
 
     commands.spawn((
@@ -78,7 +89,10 @@ fn setup(mut commands: Commands) {
         Velocity(Vec3::ZERO),
         Acceleration(Vec3::ZERO),
         Collision,
+        CollisionBox(Vec3::new(32.0, 32.0, 0.0)),
+        Inventory::new(),
     ));
+
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -93,7 +107,13 @@ fn setup(mut commands: Commands) {
         Velocity(Vec3::ZERO),
         Acceleration(Vec3::ZERO),
         Collision,
+        CollisionBox(Vec3::new(32.0, 32.0, 0.0)),
     ));
+
+    spawn_ingredient(&mut commands, IngredientType::One);
+    spawn_ingredient(&mut commands, IngredientType::Two);
+    spawn_ingredient(&mut commands, IngredientType::Three);
+    spawn_ingredient(&mut commands, IngredientType::Four);
 }
 
 fn gravity_system(mut q_physics: Query<&mut Acceleration>) {
@@ -125,8 +145,12 @@ struct OnGroundEvent {
     entity: Entity,
 }
 
+// NOTE: Assumes everything are rectangles
 fn collision_system(
-    q_colliders: Query<(&Transform, &Sprite), (With<Collision>, Without<Player>, Without<NPC>)>,
+    q_colliders: Query<
+        (&Transform, &CollisionBox),
+        (With<Collision>, Without<Player>, Without<NPC>),
+    >,
     mut q_player: Query<
         (
             Entity,
@@ -147,12 +171,12 @@ fn collision_system(
                 player_trans.translation,
                 player_sprite.custom_size.unwrap(),
                 transform.translation,
-                sprite.custom_size.unwrap(),
+                sprite.0.truncate(),
             );
 
             if let Some(collision) = collision {
                 //info!("collision");
-                let half_size = sprite.custom_size.unwrap() / 2.0;
+                let half_size = sprite.0.truncate() / 2.0;
                 let player_half_size = player_sprite.custom_size.unwrap() / 2.0;
 
                 match collision {
@@ -220,5 +244,162 @@ fn movement_system(
         player.translation.x -= 100.0 * time_step.period.as_secs_f32();
     } else if keyboard_input.pressed(KeyCode::Right) {
         player.translation.x += 100.0 * time_step.period.as_secs_f32();
+    }
+}
+
+#[derive(Component)]
+struct Ingredient(IngredientType);
+
+#[derive(Clone, PartialEq, Eq)]
+enum IngredientType {
+    One,
+    Two,
+    Three,
+    Four,
+}
+
+fn spawn_ingredient(commands: &mut Commands, ingredient: IngredientType) {
+    let color = {
+        match ingredient {
+            IngredientType::One => Color::YELLOW_GREEN,
+            IngredientType::Two => Color::DARK_GREEN,
+            IngredientType::Three => Color::LIME_GREEN,
+            IngredientType::Four => Color::YELLOW,
+        }
+    };
+
+    let position = {
+        match ingredient {
+            IngredientType::One => Vec3::new(0.0, -300.0, 0.0),
+            IngredientType::Two => Vec3::new(200.0, -300.0, 0.0),
+            IngredientType::Three => Vec3::new(-100.0, -300.0, 0.0),
+            IngredientType::Four => Vec3::new(-80.0, -300.0, 0.0),
+        }
+    };
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color,
+                custom_size: Some(Vec2::new(32.0, 32.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(position),
+            ..default()
+        },
+        Ingredient(ingredient),
+        TriggerBox(Vec3::new(40.0, 40.0, 0.0)),
+    ));
+}
+
+#[derive(Component)]
+struct Teller;
+
+fn trigger_ingredient_system(
+    mut commands: Commands,
+    mut q: Query<(Entity, &mut Transform, &TriggerBox, &Ingredient), Without<Player>>,
+    mut q_player: Query<(Entity, &Transform, &Sprite, &mut Inventory), With<Player>>,
+) {
+    let (player, player_trans, player_sprite, mut inventory) =
+        q_player.get_single_mut().expect("Always a player");
+
+    for (ingredient, mut transform, sprite, Ingredient(ing)) in q.iter_mut() {
+        //info!("checking collisions");
+        let collision = collide(
+            player_trans.translation,
+            player_sprite.custom_size.unwrap(),
+            transform.translation,
+            sprite.0.truncate(),
+        );
+
+        if collision.is_some() {
+            if !inventory.items.contains(&Some(ing.clone())) {
+                if inventory.items[0] == None {
+                    inventory.items[0] = Some(ing.clone());
+                } else if inventory.items[1] == None {
+                    inventory.items[1] = Some(ing.clone());
+                } else if inventory.items[2] == None {
+                    inventory.items[2] = Some(ing.clone());
+                } else {
+                    // Inventory full
+                    break;
+                }
+
+                let count = inventory.items.iter().filter(|&el| el != &None).count();
+
+                let diff = match count {
+                    0 => unreachable!(),
+                    1 => -1.0,
+                    2 => 0.0,
+                    3 => 1.0,
+                    _ => unreachable!(),
+                };
+
+                commands.entity(ingredient).set_parent(player);
+                transform.scale = Vec3::new(0.5, 0.5, 0.0);
+                transform.translation = Vec3::new(10.0 * diff, 10.0, 10.0);
+            }
+        }
+    }
+}
+
+fn teller_system(
+    mut commands: Commands,
+    mut q: Query<(&mut Transform, &TriggerBox), (With<Teller>, Without<Player>)>,
+    mut q_player: Query<
+        (
+            Entity,
+            &Transform,
+            &CollisionBox,
+            &mut Inventory,
+            Option<&Children>,
+        ),
+        With<Player>,
+    >,
+    q_ingredients: Query<(Entity, &Ingredient)>,
+) {
+    let (player, player_trans, player_sprite, mut inventory, children) =
+        q_player.get_single_mut().expect("Always a player");
+
+    for (transform, sprite) in q.iter_mut() {
+        //info!("checking collisions");
+        let collision = collide(
+            player_trans.translation,
+            player_sprite.0.truncate(),
+            transform.translation,
+            sprite.0.truncate(),
+        );
+
+        if collision.is_some() {
+            if let Some(children) = children {
+                info!("Checking {:?}", children);
+                for &child in children.iter() {
+                    if let Ok((_, Ingredient(ing))) = q_ingredients.get(child) {
+                        if inventory.items[0] == Some(ing.clone()) {
+                            inventory.items[0] = None;
+                        } else if inventory.items[1] == Some(ing.clone()) {
+                            inventory.items[1] = None;
+                        } else if inventory.items[2] == Some(ing.clone()) {
+                            inventory.items[2] = None;
+                        }
+                        commands.entity(player).despawn_descendants();
+                        spawn_ingredient(&mut commands, ing.clone());
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct Inventory {
+    items: [Option<IngredientType>; 3],
+}
+
+impl Inventory {
+    fn new() -> Self {
+        Self {
+            items: [None, None, None],
+        }
     }
 }
